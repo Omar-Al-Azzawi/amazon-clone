27c7784057b8fcfd8e40175446c006484deba14c
Omar Al-Azzawi <alazzawiomar01@gmail.com>
2021-08-05 21:39:13 +0300

    create the header and style it

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./Header.css";
 import { Link } from "react-router-dom";
 import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 
 function Header() {
   return (
@@ -16,6 +17,32 @@ function Header() {
       <div className="header__search">
         <input type="text" className="header__searchInput" />
         <SearchIcon className="header__searchIcon" />
+      </div>
+      <div className="header__nav">
+        <Link to="/" className="header__link">
+          <div className="header__option">
+            <span className="header__optionLineOne">Hello</span>
+            <span className="header__optionLineTwo">Sign in</span>
+          </div>
+        </Link>
+        <Link to="/" className="header__link">
+          <div className="header__option">
+            <span className="header__optionLineOne">Returns</span>
+            <span className="header__optionLineTwo">& Orders</span>
+          </div>
+        </Link>
+        <Link to="/" className="header__link">
+          <div className="header__option">
+            <span className="header__optionLineOne">Your</span>
+            <span className="header__optionLineTwo">Prime</span>
+          </div>
+        </Link>
+        <Link to="/checkout" className="header__link">
+          <div className="shopping__optionBasket">
+            <ShoppingBasketIcon />
+            <span className="header__basketCount">0</span>
+          </div>
+        </Link>
       </div>
     </nav>
   );
